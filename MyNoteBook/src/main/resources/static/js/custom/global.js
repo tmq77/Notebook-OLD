d2b3a551a368553b9efa8daaf86901d6f36f585c
TMQ777 <taominqia@live.com>
2019-04-18 23:01:10 +0800

    1.global.js中添加 纯数字check函数 2.笔记列表添加编辑和删除按钮 3.完成删除按钮的前端功能操作

--- MyNoteBook/src/main/resources/static/js/custom/global.js
+++ MyNoteBook/src/main/resources/static/js/custom/global.js
@@ -35,22 +35,25 @@ toastr.options = {
 
 // 验证电话号码
 function isPhoneAvailable(str) {
-	var myreg = /^[1][3,4,5,7,8][0-9]{9}$/;
-	if (!myreg.test(str)) {
-		return false;
-	} else {
-		return true;
-	}
+	var myreg = /^[1][3,4,5,7,8][0-9]{9}$/g;
+	return myreg.test(str);
+}
+
+//验证半角数字
+function isAlphaNum(str) {
+	// 年份由2-9开头
+	// id最少18位
+	// ^[2-9] : 开头 2-9  -> 1位
+	// [0-9]{17,}: 中间 0-9 -> 最少17位(后面跟上终止符代表中间的数字至少要匹配17位才能结束)
+	// $终止符，此处可以不加终止符
+	var myreg = /^[2-9][0-9]{17,}$/g;
+	return myreg.test(str);
 }
 
 // 验证邮箱
 function isMailAvailable(str) {
 	var myreg = /^(\w-*\.*)+@(\w-?)+(\.\w{2,})+$/;
-	if (!myreg.test(str)) {
-		return false;
-	} else {
-		return true;
-	}
+	return myreg.test(str);
 }
 
 $(function() {
--- MyNoteBook/src/main/resources/static/js/custom/home.js
+++ MyNoteBook/src/main/resources/static/js/custom/home.js
@@ -1,10 +1,34 @@
 /**
  * home.html 2019年3月8日 23点22分
  */
+
+/**
+ * 删除笔记操作:由于生成笔记列表时该函数是拼接而成，所以必须定义在js上下文中(window作用域) 使用jquery的事件绑定亦可
+ * 
+ * @param _this
+ *            当前点击的对象
+ */
+/*
+ * let deleteNote = (_this) => { let url = $(_this).parent().prop("href");
+ * $.ajax({ url: url, type: "put", dataType : "json", cache : false, data : { id :
+ * $("#title").val(), _method : "put", }, success: () => {
+ *  }, error: () => { toastr.error("通信异常"); } }); // return false; 此处return
+ * false 链接还是会跳转，需要在外部定义onclick事件处加上return false; etc: onclick="func();return
+ * false;" }
+ */
+
+
+
 $(() => {
 	// 富文本编辑器变量
 	let textEditor;
-	// 分页控件生成 pageCount : 总页数
+	
+	/**
+	 * 分页函数
+	 * 
+	 * @param pageCount
+	 *            总页数
+	 */
 	let pagination = (pageCount) => {
 		let cur = parseInt($("#page-index").val());
 		let pre = cur - 1;
@@ -33,12 +57,15 @@ $(() => {
 		$(".pagination").append(li);
 	};
 	
+	// 全局配置属性，如果第一页数据大于等于10或者有第二页数据则显示分页工具
 	if ($(".note-list").length >= 10 || $("#page-index").val() > 1) {
 		$(".footer").show();
 		// 根据页数拼接页数按钮
 		let count = parseInt($("#page-count").text());
 		pagination(count);
 	}
+	
+	// -------------------------------------------------------------------------
 	
 	// 导航条点击事件:添加背景色
 	$("li").on("click", () => {
@@ -52,11 +79,12 @@ $(() => {
 			textEditor = editormd("my-editormd", {
 				placeholder : "记录精彩瞬间~",
 				emoji : true,
-				// width : "90%",  // 编辑器控件的宽度
-				height : "100%", // height : "100%",  如果指定px 例如 height : 640，那么不需要加引号也不用加px(默认px)，指定百分比则需要加引号
+				// width : "90%", // 编辑器控件的宽度
+				height : "100%", // height : "100%", 如果指定px 例如 height :
+									// 640，那么不需要加引号也不用加px(默认px)，指定百分比则需要加引号
 				editorTheme : "3024-day", // 设置主题颜色 具体见源码
 				theme : "default", // 设置工具栏主题颜色,default | dark
-				previewTheme : "default", // 设置预览颜色  default | dark
+				previewTheme : "default", // 设置预览颜色 default | dark
 				
 				taskList : true,
 				tex : true,
@@ -65,14 +93,17 @@ $(() => {
 				codeFold : true,
 				syncScrolling : "single",
 				path : "/editor.md/lib/",  // 控件的依赖包,因为springmvc的拦截器设置,"/"代表资源根目录，即static目录下
-				saveHTMLToTextarea : true, // / 保存 HTML 到 Textarea  会自动生成一个隐藏的Textarea保存HTML代码 控件名为 $-html-code  $代表创建编辑器的div的id值,这里是my-editormd
+				saveHTMLToTextarea : true, // / 保存 HTML 到 Textarea
+											// 会自动生成一个隐藏的Textarea保存HTML代码 控件名为
+											// $-html-code
+											// $代表创建编辑器的div的id值,这里是my-editormd
 
 				toolbarIcons : function() {
 					// Or return editormd.toolbarModes[name]; //
 					// full, simple, mini
 					// Using "||" set icons align right.
 					// 自定义按钮, "|"分隔符为一组
-					return [ /*"submit", "|",*/ "undo", "redo", "|",
+					return [ /* "submit", "|", */ "undo", "redo", "|",
 							"bold", "hr", "del", "|", "h1", "h2",
 							"h3", "h4", "h5", "h6", "|", "list-ul",
 							"list-ol", "|", "image", "code",
@@ -108,9 +139,9 @@ $(() => {
 				},
 				/* 上传图片配置 */
 				/*
-				 * imageUpload : true, imageFormats : [ "jpg",
-				 * "jpeg", "gif", "png", "bmp", "webp" ],
-				 * imageUploadURL : "${proPath }/base/blog/upFile",
+				 * imageUpload : true, imageFormats : [ "jpg", "jpeg", "gif",
+				 * "png", "bmp", "webp" ], imageUploadURL : "${proPath
+				 * }/base/blog/upFile",
 				 */
 			});
 		}
@@ -139,6 +170,36 @@ $(() => {
 		}
 		$("#frm-create").submit();
 	})
+	
+	// 删除笔记操作
+	// 注意 如果需要用到当前操作对象，
+	// 匿名函数的$(this)指window,所以有操作对象的时候,需要使用function()而不是es6语法的箭头函数
+	$(".delete").on("click", function(){
+		try {
+			let url = $(this).parent().prop("href");
+			// 找到最后出现的“/”,则后续的就是笔记的id
+			let index = url.lastIndexOf("/");
+			let id = url.substr(index + 1);
+			// 正确的id应该是18位以上的纯数字，并且以2开头
+			if (!isAlphaNum(id)) {
+				// toastr.warn("找不到该笔记"); // 这行代码会报异常，会进入catch
+				toastr.warning("找不到该笔记");
+			} else {
+				// 进入后台处理，完成处理后页面不刷新，清除当前点击删除的一条数据
+				// 必要时刻作隐藏分页以及切换到上一页处理
+				/*
+				 * $.ajax({ url: url, type: "put", dataType : "json", cache :
+				 * false, data : { id : $("#title").val(), _method : "put", },
+				 * success: () => {
+				 *  }, error: () => { toastr.error("通信异常"); }
+				 */
+			}
+		} catch (e) {
+			toastr.error("javascript异常：" + e);
+		}
+		return false;
+	})
+	
 });
 
 /**
